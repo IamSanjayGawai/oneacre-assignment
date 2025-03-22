a1585a6010142750b3c5aa555ea8426809db2791
Sanjay Gawai <sanjaygawai1002@gmail.com>
2025-03-22 14:07:10 +0530

    Loaded: Google Inital Map with Hydrabad Default Loaction

--- src/app/components/GoogleMapComponenet.tsx
+++ src/app/components/GoogleMapComponenet.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MapMarker } from '../lib/types'
 
 
@@ -11,12 +11,25 @@ interface GoogleMapProps {
 // If no markers are available, the map defaults to Hyderabad's coordinates.
 
 export default function GoogleMapComponenet({markers}: GoogleMapProps) {
+    const [map, setMap] = React.useState<google.maps.Map | null>(null); //Stores the Google Maps instance.
+    const [mapRef, setMapRef] = useState<HTMLDivElement | null>(null); //Stores a reference to the div where the map will be rendered.
   
 console.log("Markers in GoogleMapComponenet: ", markers);
+
+useEffect(() => {
+    if (mapRef && !map) {
+      const initialMap = new google.maps.Map(mapRef, {
+        center: { lat: 17.385044, lng: 78.486671 }, // Default to Hyderabad
+        zoom: 10,
+      });
+      setMap(initialMap);
+    }
+    }, [mapRef, map]);
+
   
     return (
     <div>
-Google Map 
+<div ref={setMapRef} style={{ width: '100%', height: '300px' }} />;
     </div>
   )
 }
