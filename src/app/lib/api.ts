2968b0b90fca86d86792229b350dacfa502b1e6e
Sanjay Gawai <sanjaygawai1002@gmail.com>
2025-03-22 14:54:32 +0530

    fix: two digit land size and markers showing currectly

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -17,7 +17,7 @@ export async function fetchMapMarkers(): Promise<MapMarker[]> {
           id: marker.id,
           latitude: parseFloat(marker.lat), // Convert string to number
           longitude: parseFloat(marker.long), // Convert string to number
-          total_acres: marker.total_land_size || 0, // Default to 0 if missing
+          total_acres: parseFloat((marker.total_land_size || 0).toFixed(2)), // Default to 0 if missing
           price_per_acre: marker.land_price?.price_per_acre_crore?.lakh || 0, // Default price
         }));
     } catch (error) {
