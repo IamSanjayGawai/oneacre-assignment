d4c9e83bfc03cb8001aaf6fe603588ebb426e614
Sanjay Gawai <sanjaygawai1002@gmail.com>
2025-03-24 19:34:29 +0530

    Formatted: formatted code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,28 @@
-
-import Navbar from '@/components/Navbar';
-import PropertyGrid from '../components/PropertyGrid';
-import PropertyMap from '../components/PropertyMap';
-import Footer from '@/components/Footer';
+import Navbar from "@/components/Navbar";
+import PropertyGrid from "../components/PropertyGrid";
+import PropertyMap from "../components/PropertyMap";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-<div className=" bg-gray-100 min-h-screen">
-  
-<Navbar />
-      <div className="container mx-auto py-8 px-4">
+      <div className=" bg-gray-100 min-h-screen">
+        <Navbar />
+        <div className="container mx-auto py-8 px-4">
+          <section className="mb-12">
+            <h2 className="text-2xl font-semibold mb-4">Property Map</h2>
+            <PropertyMap />
+          </section>
 
-        <section className="mb-12">
-          <h2 className="text-2xl font-semibold mb-4">Property Map</h2>
-          <PropertyMap />
-        </section>
-        
-        <section>
-          <h2 className="text-2xl font-semibold mb-4">Available Properties</h2>
-          <PropertyGrid />
-        </section>
+          <section>
+            <h2 className="text-2xl font-semibold mb-4">
+              Available Properties
+            </h2>
+            <PropertyGrid />
+          </section>
+        </div>
+        <Footer />
       </div>
-<Footer />      
-    </div>
     </>
   );
 }
